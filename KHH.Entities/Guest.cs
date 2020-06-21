14c7fc8359839cc6065331da4043d1d75994ec82
Midnight Coder <62158642+midni8coder@users.noreply.github.com>
2020-06-21 19:41:31 +0530

    entities

---
+++ KHH.Entities/Guest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KHH.Entities
+{
+    public class Guest
+    {
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public string AlternateMobile { get; set; }
+        public string HomeAddress { get; set; }
+        public string HomeCity { get; set; }
+        public int HomePinCode { get; set; }
+    }
+}
